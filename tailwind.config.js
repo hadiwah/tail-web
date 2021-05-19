fce239b5bdebef8129bb3178cdc0dc2481962048
hadiwah <wahyuhadi1296@gmail.com>
2021-05-19 15:24:16 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  fontFamily: {
+    display: ["Nunito", "sans-serif"],
+  },
+  variants: {},
+  plugins: [],
+};
